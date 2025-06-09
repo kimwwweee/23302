22748426d81773dfe2d234904ec69d1fe273ca75
kimwwweee <cheonghak.t230206@ggh.goe.go.kr>
2025-06-09 15:18:26 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,99 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2",
+      "name": "화학2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5471975254345463,
+          "pitch": 0.39876189494613534,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.5344111159187275,
+          "pitch": 0.6232744099241643,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5472148670930777,
+          "pitch": 0.3954291315886209,
+          "title": "세마나실",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.608651687175942,
+          "pitch": 0.6282948363416665,
+          "title": "아이고난",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "페미나실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
